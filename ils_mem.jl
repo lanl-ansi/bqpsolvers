f64e1abe31fbddc7c9eae345695bffc6219c88ff
ccoffrin <carleton@coffrin.com>
2019-12-30 14:04:21 -0700

    basic adaptation of memristor solver

---
+++ ils_mem.jl
@@ -0,0 +1,204 @@
+#!/usr/bin/env julia
+
+### Note ###
+#
+# code from the blog post at, https://invenia.github.io/blog/2019/09/11/memristors/
+#
+# tested on Julia v1.2
+#
+
+using ArgParse
+using JSON
+
+using LinearAlgebra
+
+function main(parsed_args)
+    #read the problem
+    file = open(parsed_args["input-file"])
+    data = JSON.parse(file)
+
+    if data["variable_domain"] != "boolean"
+        error("only boolean domains are supported. Given $(data["variable_domain"])")
+    end
+
+    idx_to_var = data["variable_ids"]
+    var_to_idx = Dict(var => idx for (idx,var) in enumerate(data["variable_ids"]))
+    n = length(idx_to_var)
+
+    #println(idx_to_var)
+    println("problem size: $(n), $(length(data["quadratic_terms"]))")
+
+    Q = zeros(n, n);
+    for qt in data["quadratic_terms"]
+        i = var_to_idx[qt["id_head"]]
+        j = var_to_idx[qt["id_tail"]]
+        c = qt["coeff"]
+        Q[i, j] = c/2
+        Q[j, i] = c/2
+    end
+
+    for lt in data["linear_terms"]
+        i = var_to_idx[lt["id"]]
+        c = lt["coeff"]
+        Q[i,i] = c
+    end 
+
+    #println(Q)
+    #println(data["offset"])
+
+    # v = [0 for i in 1:n]
+    # check_energy(Q, v)
+
+    # v = [1 for i in 1:n]
+    # check_energy(Q, v)
+
+    # v = [mod(i,2) for i in 1:n]
+    # check_energy(Q, v)
+
+    # v = [1, 0, 0, 0, 1, 1, 0, 1]
+    # check_energy(Q, v)
+
+    v = [0.5 for i in 1:n]
+    #v = [1, 0, 0, 0, 1, 1, 0, 1]
+    time_start = time()
+    weights_final, energies = memristive_opt(Q, v, total_time=50)
+    time_elapsed = time() - time_start
+
+    println(weights_final)
+    println(energies .+ data["offset"])
+
+    nodes = length(data["variable_ids"])
+    edges = length(data["quadratic_terms"])
+
+    scale = data["scale"]
+    offset = data["offset"]
+    lt_lb = -sum(abs(lt["coeff"]) for lt in data["linear_terms"])/scale
+    qt_lb = -sum(abs(qt["coeff"]) for qt in data["quadratic_terms"])/scale
+    lower_bound = lt_lb+qt_lb
+
+    best_objective = energy(Q, weights_final)
+    best_nodes = 0
+    best_runtime = time_elapsed
+    scaled_objective = scale*(best_objective+offset)
+    scaled_lower_bound = scale*(lower_bound+offset)
+
+    println("BQP_DATA, $(nodes), $(edges), $(scaled_objective), $(scaled_lower_bound), $(best_objective), $(lower_bound), $(best_runtime), $(0), $(best_nodes)")
+end
+
+
+"""
+    memristive_opt(
+        expected_returns::Vector{Float64},
+        Σ::Matrix{Float64},
+        p::Float64,
+        weights_init::Vector{<:Real};
+        α=0.1,
+        β=10,
+        δt=0.1,
+        total_time=3000,
+    )
+
+Execute optimisation via the heuristic "memristive" equation in order to find the
+optimal portfolio composition, considering an asset covariance matrix `Σ` and a
+risk parameter `p`. `reg` represents the regularisation constant for `Σ`, `α` and
+`β` parametrise the memristor state (see Equation (2)),
+`δt` is the size of the time step for the dynamical updates and `total_time` is
+the number of time steps for which the dynamics will be run.
+"""
+function memristive_opt(
+    expected_returns::Vector{Float64},
+    Σ::Matrix{Float64},
+    p::Float64,
+    weights_init::Vector{<:Real};
+    α=0.1,
+    β=10,
+    δt=0.1,
+    total_time=3000,
+)
+    n = size(weights_init, 1)
+
+    weights_series = Matrix{Float64}(undef, n, total_time)
+    weights_series[:, 1] = weights_init
+    energies = Vector{Float64}(undef, total_time)
+    energies[1] = energy(expected_returns, Σ, p, weights_series[:, 1])
+
+    # Compute resistance change ratio
+    ξ = p / 2α
+    # Compute Σ times applied voltages matrix
+    ΣS = β * (α/2 * ones(n, 1) + (p/2 + α * ξ/3) * diag(Σ) - expected_returns)
+
+    for t in 1:total_time-1
+        update = δt * (α * weights_series[:, t] - 1/β * (I(n) + ξ *
+            Σ * Diagonal(weights_series[:, t])) \ ΣS)
+        weights_series[:, t+1] = weights_series[:, t] + update
+
+        weights_series[weights_series[:, t+1] .> 1, t+1] .= 1.0
+        weights_series[weights_series[:, t+1] .< 1, t+1] .= 0.0
+
+        energies[t + 1] = energy(expected_returns, Σ, p, weights_series[:, t+1])
+    end
+
+    weights_final = weights_series[:, end]
+
+    return weights_final, energies
+end
+
+"setup parameters for standard QUBO optimization"
+memristive_opt(Σ::Matrix{Float64}, weights_init::Vector{<:Real}; kwargs...) = memristive_opt([0.0 for i in 1:size(Σ,1)], Σ, 2.0, weights_init; kwargs...)
+
+
+
+"""
+    energy(
+        expected_returns::Vector{Float64},
+        Σ::Matrix{Float64},
+        p::Float64,
+        weights::Vector{Float64},
+    )
+
+Return minus the expected return corrected by the variance of the portfolio,
+according to the Markowitz approach. `Σ` represents the covariance of the assets,
+`p` controls the risk tolerance and `weights` represent the (here binary)
+portfolio composition.
+"""
+function energy(
+    expected_returns::Vector{Float64},
+    Σ::Matrix{Float64},
+    p::Float64,
+    weights::Vector{<:Real},
+)
+    -dot(expected_returns, weights) + p/2 * weights' * Σ * weights
+end
+
+"setup parameters for standard QUBO optimization"
+energy(Σ::Matrix{Float64}, weights::Vector{<:Real}) = energy([0.0 for i in 1:size(Σ,1)], Σ, 2.0, weights)
+
+"checks that qubo evalution matches energy function evaluation"
+function check_energy(Q, assignment)
+    eval_q = assignment' * Q * assignment
+    eval_energy = energy(Q, assignment)
+    @assert(isapprox(eval_q, eval_energy))
+end
+
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--input-file", "-f"
+            help = "the data file to operate on (.json)"
+            required = true
+        "--time-limit", "-t"
+            help = "puts a time limit on the sovler"
+            arg_type = Float64
+        "--show-solution", "-s"
+            help = "print the solution"
+            action = :store_true
+    end
+
+    return parse_args(s)
+end
+
+if isinteractive() == false
+    main(parse_commandline())
+end
