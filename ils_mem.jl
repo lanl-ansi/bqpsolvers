4c2d77545dfd1de81bd8f7dd9ebed1e5d8343070
ccoffrin <carleton@coffrin.com>
2020-01-04 17:11:18 -0700

    to ils mem, add runtime limit, random restart, add regularisation term, early termination on convergence

--- ils_mem.jl
+++ ils_mem.jl
@@ -5,14 +5,20 @@
 # code from the blog post at, https://invenia.github.io/blog/2019/09/11/memristors/
 #
 # tested on Julia v1.2
+# test with: include("ils_mem.jl"); main(Dict("random-seed" => nothing, "max-steps"=>100, "show-solution" => true, "runtime-limit"=>1.0, "input-file" => "---"))
 #
 
 using ArgParse
 using JSON
 
+using Random
 using LinearAlgebra
 
 function main(parsed_args)
+    if !isnothing(parsed_args["random-seed"])
+        Random.seed!(parsed_args["random-seed"])
+    end
+
     #read the problem
     file = open(parsed_args["input-file"])
     data = JSON.parse(file)
@@ -45,10 +51,14 @@ function main(parsed_args)
         h[i] = c
     end
 
-    # optional regularisation term
-    # for i in 1:n
-    #     Q[i,i] = 1e-4
-    # end
+    #optional regularisation term
+    for i in 1:n
+        Q[i,i] = 1e-4
+    end
+
+    # complication run
+    memristive_opt(h, Q, 0.1, [0.5 for i in 1:n], total_time=1)
+
 
     #v = [0 for i in 1:n]
     #check_encoding(h, Q, v)
@@ -59,25 +69,46 @@ function main(parsed_args)
     #v = [mod(i,2) for i in 1:n]
     #check_encoding(h, Q, v, data)
 
-    # tuneable paprameter
+    # a tunable parameter
     p = 0.1
 
-    v = [0.5 for i in 1:n]
+    restarts = 0
+    best_assignment = Dict(idx_to_var[i] => 0.0 for i in 1:n)
+    best_energy = calc_energy(data, best_assignment)
+    print("($(best_energy))")
+
     time_start = time()
-    #weights_final, energies = memristive_opt(Q, v, total_time=100, p=0.1)
-    weights_final, energies = memristive_opt(h, Q, p, v, total_time=100)
+    while time() - time_start < parsed_args["runtime-limit"]
+        v = [Random.rand() for i in 1:n]
+
+        weights_final, energies = memristive_opt(h, Q, p, v, total_time=parsed_args["max-steps"])
+
+        #println("energies trace: $energies")
+        #println("weights: $weights_final")
+
+        assignment = Dict(idx_to_var[i] => weights_final[i] for i in 1:n)
+        energy = calc_energy(data, assignment)
+
+        if energy < best_energy
+            best_energy = energy
+            best_assignment = assignment
+            print("($(best_energy))")
+        end
+
+        restarts += 1
+        print("R")
+    end
+    println()
+
     time_elapsed = time() - time_start
 
-    #println("energies trace: $energies")
-    #println("final assignment: $weights_final")
+    println("restarts: $restarts")
 
-    assignment = Dict(idx_to_var[i] => weights_final[i] for i in 1:n)
-    energy = calc_energy(data, assignment)
-    println("final energy eval: $energy")
+    println("final energy eval: $best_energy")
 
     if parsed_args["show-solution"]
         println()
-        for (i,v) in sort(collect(assignment), by=first)
+        for (i,v) in sort(collect(best_assignment), by=first)
             println("$(i) - $(v)")
         end
     end
@@ -92,8 +123,8 @@ function main(parsed_args)
     qt_lb = -sum(abs(qt["coeff"]) for qt in data["quadratic_terms"])/scale
     lower_bound = lt_lb+qt_lb
 
-    best_objective = energy
-    best_nodes = 0
+    best_objective = best_energy
+    best_nodes = restarts
     best_runtime = time_elapsed
     scaled_objective = scale*(best_objective+offset)
     scaled_lower_bound = scale*(lower_bound+offset)
@@ -150,19 +181,37 @@ function memristive_opt(
     ΣS = β * (α/2 * ones(n, 1) + (p/2 + α * ξ/3) * diag(Σ) - expected_returns)
 
     #println(inv(Σ)*ΣS)
-
-    for t in 1:total_time-1
+    prev_update = zeros(n)
+    t = 1
+    while t < total_time
+    #for t in 1:total_time-1
         update = δt * (α * weights_series[:, t] - 1/β * (I(n) + ξ *
             Σ * Diagonal(weights_series[:, t])) \ ΣS)
+
+        #println(norm(update .- prev_update))
+        if norm(update .- prev_update) <= 1e-6
+            break # solver converged
+        else
+            #println(norm(update .- prev_update), " ", update)
+        end
+        prev_update = update
+
         weights_series[:, t+1] = weights_series[:, t] + update
 
         weights_series[weights_series[:, t+1] .> 1, t+1] .= 1.0
         weights_series[weights_series[:, t+1] .< 1, t+1] .= 0.0
 
         energies[t + 1] = energy(expected_returns, Σ, p, weights_series[:, t+1])
+        #println(weights_series[:, t+1])
+        #println(energies[t + 1])
+        t += 1
     end
 
-    weights_final = weights_series[:, end]
+    #println(t)
+    weights_final = weights_series[:, t]
+
+    #println(weights_final)
+    #println(energies)
 
     return weights_final, energies
 end
@@ -233,11 +282,19 @@ function parse_commandline()
             help = "the data file to operate on (.json)"
             required = true
         "--runtime-limit", "-t"
-            help = "puts a time limit on the sovler"
+            help = "puts a time limit on the solver"
             arg_type = Float64
+            default = 10.0
         "--show-solution", "-s"
             help = "print the solution"
             action = :store_true
+        "--random-seed"
+            help = "fixes the random number generator seed"
+            arg_type = Int
+        "--max-steps"
+            help = "set the maximum number of steps in the memristor evolution"
+            arg_type = Int
+            default = 100
     end
 
     return parse_args(s)
